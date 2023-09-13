25859e2811929a7aeca5e38e6f25315aa0d68847
Davi Machado <machadondavi@gmail.com>
2023-09-13 20:36:12 -0300

    mock tweets service

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.twitter.to.kafka.service.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
